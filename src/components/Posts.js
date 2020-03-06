05ec1ce365e203012135fb59443450e3da2282a8
Naftaliev <n2i1a95@yandex.ru>
2020-03-07 01:24:23 +0300

    states, posts from json array

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,72 @@
 import React, {Component} from 'react';
-import Post from'./Post';
-
+import InstaService from '../services/instaService';
+import User from './User';
+import ErrorMessage from './Error';
 
 export default class Posts extends Component {
+	InstaService = new InstaService();
+	state = {
+		posts: [],
+		error: false
+	}
+
+	componentDidMount() {
+		this.updatePosts();
+	}
+
+	updatePosts() {
+		this.InstaService.getAllPosts()
+		.then(this.onPostsLoaded)
+		.catch(this.onError);
+	}
+
+	onPostsLoaded = (posts) => {
+		this.setState({
+			posts: posts,
+			error: false
+		})
+	}
+
+	onError = (err) => {
+		this.setState({
+			error: true
+		})
+	}
+
+	renderItems(arr) {
+		return arr.map(item => {
+			const {name, altname, photo, src, alt, descr, id} = item;
+
+			return(
+				<div key={id} className="post">
+					<User 
+					src={photo}
+					alt={altname}
+					name={name}
+					min/>
+						<img src={src} alt={alt}></img>
+					<div className="post__name">
+						{name}
+					</div>
+					<div className="post__description">
+						{descr}
+					</div>
+				</div>
+			)
+
+		});
+ }
 	render() {
+		const {error, posts} = this.state;
+
+		if (error) {
+			return <ErrorMessage/>
+		}
+
+		const items = this.renderItems(posts);
 		return (
 				<div className="left">
-					<Post src="https://im0-tub-ru.yandex.net/i?id=b349d936e4d033184ee2f6012452f4c2&n=13" alt="inst"/>
-					<Post src="https://avatars.mds.yandex.net/get-pdb/2303218/c8d4fa56-4079-44e7-ae4c-109f192aedd7/s1200" alt="second"/>
+				 {items}
 				</div>
 			)
 	}
